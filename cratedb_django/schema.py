1193bfa6532848dd38500b267df1e687a3e0d77f
surister <surister98@gmail.com>
2024-11-13 14:21:46 +0100

    Set alter columns as noop, remove 'CASCADE' from other ops

--- cratedb_django/schema.py
+++ cratedb_django/schema.py
@@ -2,7 +2,24 @@ from django.db.backends.base.schema import BaseDatabaseSchemaEditor
 
 
 class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
-    sql_create_index = "SELECT 1"
+    # TODO DOCUMENT CAVEAT: IF YOU START WITH A DJANGO MIGRATIONS CREATED BY OTHER DATABASE LIKE POSTGRES,
+    # NEW MIGRATIONS WITH NO-OP operations like drop constraint, might produce confusing behaviour, you might
+    # expect things that will be no-op, like drop constraints.
+
+    # TODO pgdiff
+    # TODO, is this bug? why isn't supports_deferrable_unique_constraints respected on table creation?
+    # create table contentypes to reproduce
+
+    sql_create_unique = ('select 1')
+
+    sql_alter_column_type = "SELECT 1"
+    sql_alter_column_null = "SELECT 2"
+    sql_alter_column_not_null = "SELECT 3"
+    sql_alter_column_default = "SELECT 4"
+    sql_alter_column_no_default = "SELECT 5"
+
+    sql_delete_column = "ALTER TABLE %(table)s DROP COLUMN %(column)s"
+    sql_delete_table = "DROP TABLE %(table)s"
 
     def add_index(self, model, index):
         return None
