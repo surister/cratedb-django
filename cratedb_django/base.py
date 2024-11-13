57fc3093b19c399b22c713ad069ccafc54c5e1ce
surister <surister98@gmail.com>
2024-11-13 14:20:15 +0100

    make qparam '?' the default placeholder for all statements

--- cratedb_django/base.py
+++ cratedb_django/base.py
@@ -1,9 +1,13 @@
+from collections.abc import Mapping
+from itertools import tee
+
 from crate.client.converter import DefaultTypeConverter
 from crate.client.cursor import Cursor
 from crate.client.connection import Connection
 from django.core.exceptions import ImproperlyConfigured
 
 from django.db.backends.base.base import BaseDatabaseWrapper
+from django.utils.regex_helper import _lazy_re_compile
 
 from .client import DatabaseClient
 from .creation import DatabaseCreation
@@ -64,6 +68,23 @@ class DatabaseWrapper(BaseDatabaseWrapper):
         "UUIDField": "uuid",
     }
 
+    operators = {
+        "exact": "= %s",
+        "iexact": "= UPPER(%s)",
+        "contains": "LIKE %s",
+        "icontains": "LIKE UPPER(%s)",
+        "regex": "~ %s",
+        "iregex": "~* %s",
+        "gt": "> %s",
+        "gte": ">= %s",
+        "lt": "< %s",
+        "lte": "<= %s",
+        "startswith": "LIKE %s",
+        "endswith": "LIKE %s",
+        "istartswith": "LIKE UPPER(%s)",
+        "iendswith": "LIKE UPPER(%s)",
+    }
+
     def _set_autocommit(self, autocommit):
         with self.wrap_database_errors:
             self.connection.autocommit = autocommit
@@ -85,4 +106,50 @@ class DatabaseWrapper(BaseDatabaseWrapper):
         return Connection(**conn_params)
 
     def create_cursor(self, name=None):
-        return Cursor(self.connection, DefaultTypeConverter())
+        return CrateDBCursorWrapper(self.connection, DefaultTypeConverter())
+
+
+FORMAT_QMARK_REGEX = _lazy_re_compile(r"(?<!%)%s")
+
+
+# Inspired by SQLITE driver
+class CrateDBCursorWrapper(Cursor):
+    """
+    Django uses the "format" and "pyformat" styles, but Python's sqlite3 module
+    supports neither of these styles.
+
+    This wrapper performs the following conversions:
+
+    - "format" style to "qmark" style
+    - "pyformat" style to "named" style
+
+    In both cases, if you want to use a literal "%s", you'll need to use "%%s".
+    """
+
+    def execute(self, query, params=None):
+        if params is None:
+            return super().execute(query)
+        # Extract names if params is a mapping, i.e. "pyformat" style is used.
+        param_names = list(params) if isinstance(params, Mapping) else None
+        query = self.convert_query(query, param_names=param_names)
+        return super().execute(query, params)
+
+    def executemany(self, query, param_list):
+        # Extract names if params is a mapping, i.e. "pyformat" style is used.
+        # Peek carefully as a generator can be passed instead of a list/tuple.
+        peekable, param_list = tee(iter(param_list))
+        if (params := next(peekable, None)) and isinstance(params, Mapping):
+            param_names = list(params)
+        else:
+            param_names = None
+        query = self.convert_query(query, param_names=param_names)
+        return super().executemany(query, param_list)
+
+    def convert_query(self, query, *, param_names=None):
+        if param_names is None:
+            # Convert from "format" style to "qmark" style.
+            # todo pgdiff
+            return FORMAT_QMARK_REGEX.sub("?", query).replace("%%", "%")
+        else:
+            # Convert from "pyformat" style to "named" style.
+            return query % {name: f":{name}" for name in param_names}
---
+++ cratedb_django/compiler.py
@@ -0,0 +1,20 @@
+from django.db.models.sql.compiler import SQLCompiler # noqa -- Import needed for re-imports.
+
+from django.db.models.sql.compiler import SQLInsertCompiler, SQLUpdateCompiler, \
+    SQLAggregateCompiler, SQLDeleteCompiler, SQLCompiler
+
+
+class SQLInsertCompiler(SQLInsertCompiler):
+    pass
+
+
+class SQLDeleteCompiler(SQLDeleteCompiler):
+    pass
+
+
+class SQLUpdateCompiler(SQLUpdateCompiler):
+    pass
+
+
+class SQLAggregateCompiler(SQLAggregateCompiler):
+    pass
