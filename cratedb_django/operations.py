ec00fc71f82f62723a7a8934e710ab08deee623b
surister <surister98@gmail.com>
2025-04-21 16:29:21 +0200

    Implement sql_flush operation

--- cratedb_django/operations.py
+++ cratedb_django/operations.py
@@ -8,3 +8,6 @@ class DatabaseOperations(BaseDatabaseOperations):
         if name.startswith('"') and name.endswith('"'):
             return name  # Quoting once is enough.
         return f'"{name}"'
+
+    def sql_flush(self, style, tables, *, reset_sequences=False, allow_cascade=False):
+        return [f'DELETE FROM {table}' for table in tables]
