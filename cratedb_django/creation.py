ae414f8a6a94f535bf65903f07809080f462105e
surister <surister98@gmail.com>
2025-04-21 16:28:11 +0200

    Set `destroy_test_db` and `_create_test_db` as NO-OPs

--- cratedb_django/creation.py
+++ cratedb_django/creation.py
@@ -2,4 +2,6 @@ from django.db.backends.base.creation import BaseDatabaseCreation
 
 
 class DatabaseCreation(BaseDatabaseCreation):
-    pass
+    def destroy_test_db(self, old_database_name=None, verbosity=1, keepdb=False, suffix=None): pass
+    def _create_test_db(self, *a, **kw):
+        pass
