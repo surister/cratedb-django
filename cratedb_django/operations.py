0ecdbf0e26fff9c1a0243612fa1481e391a346ec
surister <surister98@gmail.com>
2025-04-25 21:43:12 +0200

    Add typehint

--- cratedb_django/operations.py
+++ cratedb_django/operations.py
@@ -4,10 +4,10 @@ from django.db.backends.base.operations import BaseDatabaseOperations
 class DatabaseOperations(BaseDatabaseOperations):
     compiler_module = "cratedb_django.compiler"
 
-    def quote_name(self, name):
+    def quote_name(self, name) -> str:
         if name.startswith('"') and name.endswith('"'):
             return name  # Quoting once is enough.
         return f'"{name}"'
 
-    def sql_flush(self, style, tables, *, reset_sequences=False, allow_cascade=False):
+    def sql_flush(self, style, tables, *, reset_sequences=False, allow_cascade=False) -> list[str]:
         return [f"DELETE FROM {table}" for table in tables]
