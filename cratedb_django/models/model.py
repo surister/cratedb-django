870b62f7e5791a086865ba29731cba8e39103c1a
surister <surister98@gmail.com>
2025-04-25 21:44:59 +0200

    Add custom attributes to CrateModel's Meta class

---
+++ cratedb_django/models/model.py
@@ -0,0 +1,50 @@
+from django.db import models, connection
+from django.db.models.base import ModelBase
+
+# Tuple of all the extra options a CrateModel Meta class has.
+# (name, default_value)
+CRATE_META_OPTIONS = (
+    ("auto_refresh", False),  # Automatically refresh a table on inserts.
+)
+
+
+class MetaCrate(ModelBase):
+    def __new__(cls, name, bases, attrs, **kwargs):
+        crate_attrs = {}
+
+        # todo document
+
+        try:
+            meta = attrs['Meta']
+            for crate_attr in CRATE_META_OPTIONS:
+                attr_name = crate_attr[0]
+                if attr_name in meta.__dict__:
+                    crate_attrs[attr_name] = meta.__dict__[attr_name]
+                    delattr(meta, attr_name)
+
+        except KeyError:
+            # Has no meta class
+            pass
+
+        o = super().__new__(cls, name, bases, attrs, **kwargs)
+
+        # Return back the crate_attrs we took from meta to the already created object.
+        for k, v in crate_attrs.items():
+            setattr(o._meta, k, v)
+        return o
+
+class CrateModel(models.Model, metaclass=MetaCrate):
+    """
+    A base class for Django models with extra CrateDB specific functionality,
+
+    Methods:
+        refresh: Refreshes the given model (table)
+    """
+
+    @classmethod
+    def refresh(cls):
+        with connection.cursor() as cursor:
+            cursor.execute(f"refresh table {cls._meta.db_table}")
+
+    class Meta:
+        abstract = True
